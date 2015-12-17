8661dd2bd8e65bc6c1af3d2f127db9d63c4d1dd9
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-12-17 21:11:35 +0100

    Update language config

--- com.galenframework.layout.spec.parent/com.galenframework.layout.spec/src/com/galenframework/parser/antlr/SpecTokenSource.java
+++ com.galenframework.layout.spec.parent/com.galenframework.layout.spec/src/com/galenframework/parser/antlr/SpecTokenSource.java
@@ -14,7 +14,7 @@ public class SpecTokenSource extends AbstractIndentationTokenSource {
 	
 	@Override
 	protected boolean shouldSplitTokenImpl(Token token) {
-		return token.getType() == InternalSpecParser.RULE_NL;
+		return token.getType() == InternalSpecParser.RULE_WS;
 	}
 
 	@Override
